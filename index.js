d3b4c107e41dbb067034b7299dd1600e17113ddb
oxho-dev <jin.combalicer711@gmail.com>
2025-01-01 06:01:31 +0800

    index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["BOT NI SAITO", "KUPAL KABA MAN?"];
+const statusMessages = ["LUHOD BITCH", "KUPAL KABA MAN?"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
